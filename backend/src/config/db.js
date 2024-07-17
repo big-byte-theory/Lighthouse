ef940042d84422427b84881a291b9e5b0ec80d6a
Amy Leung <leungmya@gmail.com>
2024-07-17 17:52:11 +0100

    chore: add crossenv and production script

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,7 +9,7 @@ export default class Database {
 
   connect = async () => {
     try {
-      await mongoose.connect(this.#uri);
+      await mongoose.connect(this.#uri, { useNewUrlParser: true , useUnifiedTopology: true });
       return console.log('MongoDB Connected...');
     } catch (err) {
       console.error(err.message);
